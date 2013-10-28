210fff05c7f6fdae6250cc32144d0b67f397e34e
Mushin No Mushin <mushinnomushin@gmail.com>
2013-10-27 20:10:53 -0500

    It's ugly, but it's a start.

--- DOSome.rb
+++ DOSome.rb
@@ -1,2 +1,30 @@
-#Title
-print "DOSome - Your one stop shop for all your DoS & DDoS needs\nUsage: ruby DOSome.rb <url>\n"
+#! /usr/bin/ruby
+
+## Title
+
+print "DOSome - Your one stop shop for all your DoS & DDoS needs\nUsage: ruby DOSome.rb <url>\n\n"
+
+## Command input
+
+commandString=gets.chomp
+print "#{command}"
+
+## Switches
+
+case commandSwitches
+
+   when --dns
+      targetDns=gets.chomp
+
+   when --url
+      url=gets.chomp
+
+   when --ip
+      ipAddr=gets.chomp
+
+   when --number
+      number=gets.chomp
+
+   when --speed
+      speed=gets.chomp
+end
--- informator.rb
+++ informator.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/ruby
 
+case commandSwitches
+
+   when dnsMissing
+
+   when ipAddrmissing
+
+   when robotsGet
+
+   when spiderWeight
+
+   when 
